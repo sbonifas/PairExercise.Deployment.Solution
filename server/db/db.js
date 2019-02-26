8fb4d629df24c46ed6ba3001803cdcbaff109898
Steve Bonifas <stevenbonifas@gmail.com>
2019-02-26 12:58:19 -0600

    test Travis

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
-const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name
+// const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name
+const dbName = `${pkg.name}-test`
 const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
 const client = new Sequelize(dbUrl, { logging: false, operatorsAliases: false })
 
